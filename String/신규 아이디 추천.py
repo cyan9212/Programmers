7355eab5c05229411619e4f2433d5913289a33f6
cyan9212 <42201371+cyan9212@users.noreply.github.com>
2021-11-14 20:04:26 +0900

    Create 신규 아이디 추천.py

---
+++ String/신규 아이디 추천.py
@@ -0,0 +1,27 @@
+def solution(new_id):
+    new_id = new_id.lower() #1단계
+    length = len(new_id)
+    
+    answer = '' #2단계
+    for word in new_id:
+        if word.isalnum() or word in '-_.':
+            answer += word
+    
+    while '..' in answer: #3단계
+        answer = answer.replace('..','.')
+    
+    answer = answer[1:] if answer[0] == '.' and len(answer) > 1 else answer #4단계
+    answer = answer[:-1] if answer[-1] == '.' else answer
+     
+    if answer == '': #5단계
+        answer = 'a'
+    
+    if len(answer)>=16: #6단계
+        answer = answer[:15]
+    if answer[-1] == '.':
+        answer = answer[:-1]
+    
+    while len(answer)<=2: #7단계
+        answer = answer+answer[-1]
+        
+    return answer
